69ad3be68529f7f566e440caf9f60bca26f781a5
Stig Boye Andersen <stigboyeandersen@gmail.com>
2023-05-04 14:46:53 +0200

    -

--- check-reminders.py
+++ check-reminders.py
@@ -5,10 +5,14 @@ import json
 import sqlite3
 from datetime import datetime
 
-def getRepositories():
+
+# Reads all the repositories 
+def loadAndValidateRepositoriesCofiguration():
     with open('./admonitio/repositories.json', 'r') as f:
-        repositoriesString = f.read()
-        return json.loads(repositoriesString)
+        repositoriesJSON = f.read()
+        return json.loads(repositoriesJSON)
+
+    
 
 #create a function that creates an in memory sql lite database
 def createDatabase():
@@ -24,13 +28,12 @@ def createTables(conn):
     c.execute('''CREATE TABLE notifications(id integer primary key, notification integer, reminder_id integer, FOREIGN KEY(reminder_id) REFERENCES reminders(id))''')
     conn.commit()
 
-def hasAdmonitioFrontmatter(mdFrontmatter):
-    if not mdFrontmatter.get('admonitio'):
+def hasValidAdmonitioFrontmatter(frontMatter):
+    if not frontMatter.get('admonitio'):
         return False
     
-    return True
+    admonitioFrontmatter  = frontMatter.get('admonitio')
 
-def isAdmonitioFrontmatterValid(admonitioFrontmatter):
     if not admonitioFrontmatter.get('title'):
         return False
 
@@ -42,45 +45,56 @@ def isAdmonitioFrontmatterValid(admonitioFrontmatter):
 
     return True
 
-#Parse md files and insert into database
-def parseMdFileAndInsertIntoDb(conn, directory):
+def isAdmonitioFrontmatterValid(admonitioFrontmatter):
+    
+    return True
+
+# Parse md files and insert into database
+def parseMarkdownFiles(conn, markdownDirectory):
     c = conn.cursor()
 
-    docsDirectory = directory + '/docs'
-
-    for filename in os.listdir(docsDirectory):
+    for filename in os.listdir(markdownDirectory):
         if filename.endswith('.md'):
-            with open(docsDirectory + '/' + filename, 'r') as f:
+            with open(markdownDirectory + '/' + filename, 'r') as f:
                 frontMatter = frontmatter.loads(f.read())
 
-                if hasAdmonitioFrontmatter(frontMatter):
+                if hasValidAdmonitioFrontmatter(frontMatter):
+                    print('A valid Admonitio frontmatter found in ' + markdownDirectory + "/" + filename)
+
                     admonitioFrontMatter  = frontMatter.get('admonitio')
-                    if isAdmonitioFrontmatterValid(admonitioFrontMatter):
-                        title = admonitioFrontMatter.get('title')
-                        dueDate = admonitioFrontMatter.get('dueDate')
-                        convertedDueDate = datetime.strptime(dueDate, '%d.%m.%Y')
+                    title = admonitioFrontMatter.get('title')
+                    dueDate = admonitioFrontMatter.get('dueDate')
+                    convertedDueDate = datetime.strptime(dueDate, '%d.%m.%Y')
 
-                        c.execute("INSERT INTO reminders(title, dueDate) VALUES(?, ?)", (title, convertedDueDate.strftime('%Y-%m-%d')))
+                    c.execute("INSERT INTO reminders(title, dueDate) VALUES(?, ?)", (title, convertedDueDate.strftime('%Y-%m-%d')))
 
-                        reminderId = c.lastrowid
-                        notifications = admonitioFrontMatter.get('notifications')    
-                        for notification in notifications:
-                            c.execute("INSERT INTO notifications(notification, reminder_id) VALUES(?, ?)", (notification, reminderId))
-                    else:
-                        print('Invalid Admonitio frontmatter for document: ' + filename)
+                    reminderId = c.lastrowid
+                    notifications = admonitioFrontMatter.get('notifications')    
+                    for notification in notifications:
+                        c.execute("INSERT INTO notifications(notification, reminder_id) VALUES(?, ?)", (notification, reminderId))
+                else:
+                    print('No valid Admonitio frontmatter found in ' + markdownDirectory + "/" + filename)
     conn.commit()
 
 if __name__ == "__main__":
     conn = createDatabase()
     createTables(conn)
 
-    repositories = getRepositories()
-    for repo in repositories:
-        parseMdFileAndInsertIntoDb(conn, repo.get('name'))
+    repositories = loadAndValidateRepositoriesCofiguration()
+    for repository in repositories:
+        markdownDirectory = repository.get('name') + '/docs'
+        print('Parsing all markdown files in repo ' + repository.get('name') + ' under ' + markdownDirectory)
 
-    #select all reminders and calculate when the 
+        
+        parseMarkdownFiles(conn, markdownDirectory)
+
+
     c = conn.cursor()
     c.execute("SELECT title, dueDate, DATE(dueDate,'-' || notification || ' day') as notificationDay \
                FROM reminders join notifications on reminders.id = notifications.reminder_id \
+               WHERE notificationDay >= date('now') \
                ORDER BY notificationDay ASC")
-    print(c.fetchall())
+    
+    reminders = c.fetchall()
+    for reminder in reminders:
+        print(reminder)
